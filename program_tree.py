ccd119f4920725248ceec1b19cfe6463d08e3183
Charlotte Pierce <charlotte@malformed-bits.com>
2013-06-02 20:58:36 +1000

    Added method to generate flat list of nodes in a program tree.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- program_tree.py
+++ program_tree.py
@@ -5,6 +5,28 @@ class ProgramTree():
 		self.start_node = first_node
 		self.curr_node = self.start_node
 
+	def node_list(self):
+		''' Generate and return a flat list of all nodes in the tree.
+
+		Uses depth-first search. '''
+
+		queue = [self.start_node]
+		result = []
+		seen = []
+		while len(queue) > 0:
+			curr_node = queue.pop()
+			if not curr_node:
+				continue
+
+			result.append(curr_node)
+			if curr_node.conditional:
+				queue.append(curr_node.true_branch)
+				queue.append(curr_node.false_branch)
+			else:
+				queue.append(curr_node.next_node)
+
+		return result
+
 	def random_node(self):
 		''' Return a random node from the program tree.
 
@@ -14,7 +36,7 @@ class ProgramTree():
 
 		'''
 
-		return choice(self.nodes)
+		return choice(self.node_list())
 
 class ProgramTreeNode():
 	def __init__(self, parent_node, action, conditional=False):
@@ -27,4 +49,7 @@ class ProgramTreeNode():
 			self.false_branch = None
 		else:
 			self.next_node = None
+
+	def __str__(self):
+		return self.action
 
