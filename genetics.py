ade83b54171d0d6e15f13cb185c722083ac67499
Charlotte Pierce <charlotte@malformed-bits.com>
2013-06-03 15:41:51 +1000

    Added random selection of genetic operation.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- genetics.py
+++ genetics.py
@@ -1,6 +1,6 @@
 import pyglet
 
-from random import randint
+from random import randint, random
 import os
 
 import util
@@ -8,7 +8,7 @@ from agent import Agent, Guard
 from sim_window import SimWindow
 
 class Experiment():
-	def __init__(self, map_file, population_size, max_steps, iterations=5):
+	def __init__(self, map_file, population_size, max_steps, iterations=5, reproduction_prob=0.495, crossover_prob=0.495, mutation_prob=0.01):
 		''' Set up a new experiement.
 
 		args
@@ -21,10 +21,18 @@ class Experiment():
 
 		'''
 
+		# check probabilities given sum to 1
+		if not ((reproduction_prob + crossover_prob + mutation_prob) == 1.0):
+			print '!!! Bad genetic operation probabilities. Check parameters. !!!'
+			return
+
 		self.environment = util.create_map(map_file)
 		self.population_size = population_size
 		self.max_steps = max_steps
 		self.iterations = iterations
+		self.reproduction_prob = reproduction_prob
+		self.crossover_prob = crossover_prob
+		self.mutation_prob = mutation_prob
 		# initialise agents
 		self.population = self._init_population(self.environment, self.population_size)
 		self.guard = None
@@ -35,23 +43,70 @@ class Experiment():
 		''' Run the experiment. '''
 
 		for iteration in range(self.iterations):
-			print 'Iteration:', iteration + 1
-			logger = Logger(iteration + 1, len(self.population))
-
-			distances = [] # list of (agent, distance_from_goal) pairs
-			for agent in self.population:
-				sim = SimWindow(agent, self.guard, self.environment, self.max_steps, graphics_on=False)
-				pyglet.app.run()
-				distance_from_goal = agent.tile.distance(self.environment.goal)
-				logger.log_performance(agent, distance_from_goal)
-				distances.append((agent, distance_from_goal))
-
-				agent.reset()
-
-			best = min(distances, key=lambda p: p[1])
+			# calculate
+			results = self._run_iteration(iteration)
+			best = min(results, key=lambda p: p[1])
 			print 'Closest distance:', best[1]
+			# apply genetics
+			self._apply_genetic_operation()
 
-			logger.close()
+	def _run_iteration(self, iteration):
+		''' Run a single iteration, logging the results.
+
+		All agents are reset to their starting position and top of their program
+		tree after.
+
+		args
+		----
+			iteration: The iteration number.
+
+		return
+		------
+			Results of the iteration, as a list of (agent, distance_from_goal) pairs.
+
+		'''
+
+		print 'Iteration:', iteration + 1
+		logger = Logger(iteration + 1, len(self.population))
+
+		distances = [] # list of (agent, distance_from_goal) pairs
+		for agent in self.population:
+			sim = SimWindow(agent, self.guard, self.environment, self.max_steps, graphics_on=False)
+			pyglet.app.run()
+			distance_from_goal = agent.tile.distance(self.environment.goal)
+			logger.log_performance(agent, distance_from_goal)
+			distances.append((agent, distance_from_goal))
+
+			agent.reset()
+
+		logger.close()
+
+		return distances
+
+	def _apply_genetic_operation(self):
+		''' Choose reproduction, crossover or mutation randomly (according to their weight)
+		and apply chosen genetic operation to the population. '''
+
+		new_population = []
+
+		while len(new_population) < self.population_size:
+			genetic_operation = self._random_genetic_operation()
+			print 'operation:', genetic_operation
+
+		self.population = new_population
+
+	def _random_genetic_operation(self):
+		''' Choose one of 'reproduction', 'crossover' or 'mutation' randomly according to their weighting. '''
+
+		weights = {'reproduction':self.reproduction_prob, 'crossover':self.crossover_prob, 'mutation':self.mutation_prob}
+		random_num = random()
+		weight_total = 0
+		for operation in weights:
+			weight = weights[operation]
+
+			weight_total += weight
+			if weight_total > random_num:
+				return operation
 
 	def _init_population(self, environment, size):
 		''' Create the initial agent population.
